6ad0a0224a2de0439bace1696f23f6db6ce5c2fc
Philihp Busby <philihp@gmail.com>
2019-11-25 21:49:19 +0000

    use a supervisor to restart application

--- lib/welcome2_constants/application.ex
+++ lib/welcome2_constants/application.ex
@@ -1,5 +1,13 @@
 defmodule Welcome2Constants.Application do
   def start(_type, _args) do
-    Welcome2Constants.Dealer.start_link()
+    import Supervisor.Spec
+
+    children = [
+      worker(Welcome2Constants.Dealer, [])
+    ]
+
+    options = [name: Welcome2Constants.Supervisor, strategy: :one_for_one]
+
+    Supervisor.start_link(children, options)
   end
 end
